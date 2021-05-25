99ae0011979d0c3b6c5112ffd86faf5162f89311
venupalabandla <venupalabanla@gmail.com>
2021-05-25 10:36:39 +0530

    initial changes on html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'SampleApp';
+firstName:string="Ranjith";
+lastName:string="Paleti";
+age:number=29;
+receiveNewsLetter:boolean=true;
+gender:string="male";
+country:string="india";
+address:string="https://www.facebook.com";
+changeData(){
+
+this.firstName="Kumar";
+this.lastName="Jhon";
+this.age=33;
+this.receiveNewsLetter=false;
+this.gender="male";
+this.country="US";
+this.address="https://mail.google.com/";
+
+}
+
 }
